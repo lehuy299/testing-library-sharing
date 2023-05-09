bca6a341498f1523204dbc6043f194dcf7d446c8
le huy <13998@student.vgu.edu.vn>
2023-05-09 16:07:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import DrinkSearch from './DrinkSearch'
+import EmployeeEmail from './EmployeeEmail'
+import Register from './Register'
+import Vote from './Vote'
+
+const App = () => <>
+    {/* <Vote totalGlobalLikes={10} /> */}
+    <DrinkSearch />
+</> 
+
+export default App
---
+++ src/DrinkSearch.test.js
@@ -0,0 +1,99 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import DrinkSearch from './DrinkSearch'
+
+const mockServer = setupServer(
+  rest.get(
+    'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          drinks: [
+            {
+              idDrink: 1,
+              strDrinkThumb: './images/thumbs-down.svg',
+              strDrink: 'test drink',
+              strInstructions: 'test instructions',
+              strIngredient1: 'test ingredient'
+            }
+          ]
+        })
+      )
+    }
+  )
+)
+
+beforeAll(() => mockServer.listen())
+afterEach(() => mockServer.resetHandlers())
+afterAll(() => mockServer.close())
+
+test('renders mock drink data', async () => {
+  render(<DrinkSearch />)
+  const searchInput = screen.getByRole('searchbox')
+
+  user.type(searchInput, 'vodka, {enter}')
+
+  expect(
+    await screen.findByRole('img', { name: /test drink/i })
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', { name: /test drink/i })
+  ).toBeInTheDocument()
+  expect(screen.getByText(/test ingredient/i)).toBeInTheDocument()
+  expect(screen.getByText(/test instructions/i)).toBeInTheDocument()
+})
+
+test('renders no drink results', async () => {
+  mockServer.use(
+    rest.get(
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+      (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            drinks: null
+          })
+        )
+      }
+    )
+  )
+  render(<DrinkSearch />)
+  const searchInput = screen.getByRole('searchbox')
+
+  user.type(searchInput, 'vodka, {enter}')
+
+  expect(
+    await screen.findByRole('heading', { name: /no drinks found/i })
+  ).toBeInTheDocument()
+})
+
+test('renders service unavailable', async () => {
+  mockServer.use(
+    rest.get(
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+      (req, res, ctx) => {
+        return res(ctx.status(503))
+      }
+    )
+  )
+  render(<DrinkSearch />)
+  const searchInput = screen.getByRole('searchbox')
+
+  user.type(searchInput, 'vodka, {enter}')
+
+  expect(
+    await screen.findByRole('heading', { name: /Service unavailable/i })
+  ).toBeInTheDocument()
+})
+
+test('prevents GET request when search input empty', async () => {
+  render(<DrinkSearch />)
+  const searchInput = screen.getByRole('searchbox')
+
+  user.type(searchInput, '{enter}')
+
+  expect(screen.queryByRole('heading')).not.toBeInTheDocument()
+})
